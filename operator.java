60eec5d4df1830821c420b3a2a6df8caea21d3c4
AmaratoAyanokoji <srivastava.vishesh96@gmail.com>
2025-03-06 12:07:16 +0530

    Main

--- operator.java
+++ operator.java
@@ -3,7 +3,6 @@ public class operator {
         int a = 10, b = 5;
         boolean x = true, y = false;
 
-        // 1. Arithmetic Operators
         System.out.println("Arithmetic Operations:");
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
@@ -11,7 +10,6 @@ public class operator {
         System.out.println("a / b = " + (a / b));
         System.out.println("a % b = " + (a % b));
 
-        // 2. Relational Operators
         System.out.println("\nRelational Operations:");
         System.out.println("a == b: " + (a == b));
         System.out.println("a != b: " + (a != b));
@@ -20,7 +18,6 @@ public class operator {
         System.out.println("a >= b: " + (a >= b));
         System.out.println("a <= b: " + (a <= b));
 
-        // 3. Bitwise Operators
         System.out.println("\nBitwise Operations:");
         System.out.println("a & b: " + (a & b));
         System.out.println("a | b: " + (a | b));
@@ -30,13 +27,11 @@ public class operator {
         System.out.println("a >> 1: " + (a >> 1));
         System.out.println("a >>> 1: " + (a >>> 1));
 
-        // 4. Logical Operators
         System.out.println("\nLogical Operations:");
         System.out.println("x && y: " + (x && y));
         System.out.println("x || y: " + (x || y));
         System.out.println("!x: " + (!x));
 
-        // 5. Assignment Operators
         System.out.println("\nAssignment Operations:");
         int c = 20;
         System.out.println("c = " + c);
@@ -51,20 +46,17 @@ public class operator {
         c %= 3; // c = c % 3
         System.out.println("c %= 3: " + c);
 
-        // 6. Ternary Operator
         System.out.println("\nTernary Operator:");
         int min = (a < b) ? a : b;
         System.out.println("Minimum of a and b: " + min);
 
-        // 7. Increment and Decrement Operators
         System.out.println("\nIncrement & Decrement Operations:");
         int num = 10;
-        System.out.println("num++: " + (num++)); // Post-increment
-        System.out.println("++num: " + (++num)); // Pre-increment
-        System.out.println("num--: " + (num--)); // Post-decrement
-        System.out.println("--num: " + (--num)); // Pre-decrement
+        System.out.println("num++: " + (num++));
+        System.out.println("++num: " + (++num));
+        System.out.println("num--: " + (num--));
+        System.out.println("--num: " + (--num));
 
-        // 8. instanceof Operator
         System.out.println("\nInstanceof Operator:");
         String str = "Hello";
         System.out.println("str instanceof String: " + (str instanceof String));
